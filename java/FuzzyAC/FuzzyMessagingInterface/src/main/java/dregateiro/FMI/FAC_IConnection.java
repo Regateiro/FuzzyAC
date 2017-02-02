3204cef16406ddb9e728c356f1e218ab761a0524
Diogo José Domingues Regateiro <diogo_regateiro@hotmail.com>
2017-02-02 15:26:06 +0000

    Messaging Interface module created.

---
+++ java/FuzzyAC/FuzzyMessagingInterface/src/main/java/dregateiro/FMI/FAC_IConnection.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dregateiro.FMI;
+
+/**
+ * Interface for connection with the central messaging node.
+ * @author Regateiro
+ */
+public interface FAC_IConnection {
+    
+}
