86ba92043ac7fe09a500cfe74ee84fdb48e3eadc
Diogo José Domingues Regateiro <diogo_regateiro@hotmail.com>
2017-05-03 19:01:11 +0100

    Merge origin/master
    Conflicts:
    	java/FuzzyAC/FAC_Module_Decision/src/main/java/it/av/fac/decision/fis/FuzzyAnalyser.java

--- java/FuzzyAC/FAC_Module_Decision/src/main/java/it/av/fac/decision/fis/VDA.java
+++ java/FuzzyAC/FAC_Module_Decision/src/main/java/it/av/fac/decision/fis/VDA.java
@@ -25,18 +25,20 @@ import net.sourceforge.jFuzzyLogic.rule.RuleTerm;
 public class VDA {
 
     private final FIS fis;
-    
+
     /**
-     * Objects that keep the output rule terms that are used for granting/deny permission. 
+     * Objects that keep the output rule terms that are used for granting/deny
+     * permission.
      */
-    private final Map<String, List<RuleTerm>> grantingOutputRuleTerms;
-    private final Map<String, List<RuleTerm>> denyingOutputRuleTerms;
-    
+    private final Map<String, Collection<RuleTerm>> grantingOutputRuleTerms;
+    private final Map<String, Collection<RuleTerm>> denyingOutputRuleTerms;
+
     /**
-     * Objects that keep the input rule terms that are used for only granting/deny permission. 
+     * Objects that keep the input rule terms that are used for only
+     * granting/deny permission.
      */
-    private final Map<String, List<RuleTerm>> onlyGrantingInputRuleTerms;
-    private final Map<String, List<RuleTerm>> onlyDenyingInputRuleTerms;
+    private final Map<String, Collection<RuleTerm>> onlyGrantingInputRuleTerms;
+    private final Map<String, Collection<RuleTerm>> onlyDenyingInputRuleTerms;
 
     public VDA(FIS fis) {
         this.fis = fis;
@@ -50,15 +52,15 @@ public class VDA {
         //access the AC rulebook and get the associated rules.
         FunctionBlock fb_ac = this.fis.getFunctionBlock(FuzzyEvaluator.FB_ACCESS_CONTROL_PHASE_NAME);
         HashMap<String, RuleBlock> rb_ac = fb_ac.getRuleBlocks();
-        
+
         //for each rulebook
         rb_ac.keySet().stream().forEach((String permission) -> {
             RuleBlock rb = rb_ac.get(permission);
-            
+
             //fore each rule in the rulebook
             rb.getRules().stream().forEach((Rule rule) -> {
                 List<RuleTerm> r_rts = new ArrayList<>();
-                
+
                 //obtain all the rule terms from the rule into r_rts
                 setRuleTermsFromRule(rule.getAntecedents(), r_rts);
 
@@ -85,13 +87,13 @@ public class VDA {
         //access the VI rulebook and get the associated rules.
         FunctionBlock fb_vi = this.fis.getFunctionBlock(FuzzyEvaluator.FB_VARIABLE_INFERENCE_PHASE_NAME);
         HashMap<String, RuleBlock> rb_vi = fb_vi.getRuleBlocks();
-        
+
         //for each rulebook
         rb_vi.keySet().stream().forEach((String outputvar) -> {
             RuleBlock rb = rb_vi.get(outputvar);
             rb.getRules().stream().forEach((Rule rule) -> {
                 List<RuleTerm> r_rts = new ArrayList<>();
-                
+
                 //obtain all the rule terms from the rule into r_rts
                 setRuleTermsFromRule(rule.getAntecedents(), r_rts);
 
@@ -127,24 +129,43 @@ public class VDA {
                 .forEach((gperm) -> removeCommonTerms(onlyGrantingInputRuleTerms.get(gperm), onlyDenyingInputRuleTerms.get(gperm)));
     }
 
-    private void setRuleTermsFromRule(RuleExpression antecedents, List<RuleTerm> r_rts) {
+    /**
+     * Obtains all the RuleTerms from every RuleExpression and places them in
+     * r_rts.
+     *
+     * @param antecedents
+     * @param r_rts
+     */
+    private void setRuleTermsFromRule(RuleExpression antecedents, Collection<RuleTerm> r_rts) {
         Object t1 = antecedents.getTerm1();
         Object t2 = antecedents.getTerm2();
 
+        //if t1 is an expression
         if (antecedents.isFuzzyRuleExpression(t1)) {
+            //recursive call this function to deal with the expression
             setRuleTermsFromRule((RuleExpression) t1, r_rts);
         } else if (antecedents.isFuzzyRuleTerm(t1)) {
-            r_rts.add((RuleTerm) t1);
+            //if it is a ruleterm, add it to the list if it isn't there already.
+            if (!RuleTermComparator.collectionContains(r_rts, (RuleTerm) t1)) {
+                r_rts.add((RuleTerm) t1);
+            }
         }
 
+        //same deal as above but for t2
         if (antecedents.isFuzzyRuleExpression(t2)) {
             setRuleTermsFromRule((RuleExpression) t2, r_rts);
         } else if (antecedents.isFuzzyRuleTerm(t2)) {
-            r_rts.add((RuleTerm) t2);
+            if (!RuleTermComparator.collectionContains(r_rts, (RuleTerm) t2)) {
+                r_rts.add((RuleTerm) t2);
+            }
         }
     }
 
-    private void removeDuplicateRuleTerms(List<RuleTerm> ruleTermList) {
+    /**
+     * Guarantees that the given collection only has distinct ruleterms. 
+     * @param ruleTermList 
+     */
+    private void removeDuplicateRuleTerms(Collection<RuleTerm> ruleTermList) {
         List<RuleTerm> temp = new ArrayList<>();
 
         ruleTermList.stream()
@@ -154,15 +175,20 @@ public class VDA {
         ruleTermList.clear();
         ruleTermList.addAll(temp);
     }
-    
-    private void removeCommonTerms(List<RuleTerm> ruleTermList1, List<RuleTerm> ruleTermList2) {
+
+    /**
+     * Removes common terms between two collections of ruleterms.
+     * @param ruleTermList1
+     * @param ruleTermList2 
+     */
+    private void removeCommonTerms(Collection<RuleTerm> ruleTermList1, Collection<RuleTerm> ruleTermList2) {
         List<RuleTerm> temp1 = new ArrayList<>();
         List<RuleTerm> temp2 = new ArrayList<>();
 
         ruleTermList1.stream()
                 .filter((rt) -> !RuleTermComparator.collectionContains(ruleTermList2, rt))
                 .forEach((rt) -> temp1.add(rt));
-        
+
         ruleTermList2.stream()
                 .filter((rt) -> !RuleTermComparator.collectionContains(ruleTermList1, rt))
                 .forEach((rt) -> temp2.add(rt));
@@ -173,13 +199,28 @@ public class VDA {
         ruleTermList2.addAll(temp2);
     }
 
+    /**
+     * A class mean to perform comparative operations on ruleterms.
+     */
     private static class RuleTermComparator {
 
+        /**
+         * Checks if two ruleterms are the same (same variable name and same linguistic term)
+         * @param o1
+         * @param o2
+         * @return 
+         */
         public static boolean equals(RuleTerm o1, RuleTerm o2) {
             return o1.getVariable().getName().equals(o2.getVariable().getName())
                     && o1.getTermName().equals(o2.getTermName());
         }
 
+        /**
+         * Checks if a ruleterm collection contains a given ruleterm.
+         * @param col
+         * @param rt
+         * @return 
+         */
         public static boolean collectionContains(Collection<RuleTerm> col, RuleTerm rt) {
             return col.stream().anyMatch((rtincol) -> (RuleTermComparator.equals(rtincol, rt)));
         }
