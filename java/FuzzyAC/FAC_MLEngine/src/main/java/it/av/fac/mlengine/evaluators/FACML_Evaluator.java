bd6b9607531134b5475b329acc7ab4f9e8cb746b
Diogo José Domingues Regateiro <diogo_regateiro@hotmail.com>
2018-01-31 00:39:29 +0000

    Initial classes for ML Engine.

--- java/FuzzyAC/FAC_MLEngine/src/main/java/it/av/fac/mlengine/evaluators/FACML_Evaluator.java
+++ java/FuzzyAC/FAC_MLEngine/src/main/java/it/av/fac/mlengine/evaluators/FACML_Evaluator.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package it.av.fac.mlengine;
+package it.av.fac.mlengine.evaluators;
 
 /**
  *
  * @author Regateiro
  */
-public class NewClass {
+public abstract class FACML_Evaluator {
     
 }
