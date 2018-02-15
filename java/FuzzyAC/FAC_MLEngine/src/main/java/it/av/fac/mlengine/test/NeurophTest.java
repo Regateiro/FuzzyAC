0d8c081beafc9b930b986bfe85ecdaf469e95044
Diogo José Domingues Regateiro <diogo_regateiro@hotmail.com>
2018-02-15 18:42:14 +0000

    Renamed things and fixed the neural network training. (Dataset needs reworking).

--- java/FuzzyAC/FAC_MLEngine/src/main/java/it/av/fac/mlengine/test/NeurophTest.java
+++ java/FuzzyAC/FAC_MLEngine/src/main/java/it/av/fac/mlengine/test/NeurophTest.java
@@ -22,27 +22,31 @@ import org.neuroph.core.Neuron;
 import org.neuroph.core.data.DataSet;
 import org.neuroph.core.data.DataSetRow;
 import org.neuroph.core.input.Min;
+import org.neuroph.core.learning.SupervisedLearning;
 import org.neuroph.core.transfer.Gaussian;
 import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.nnet.comp.layer.InputLayer;
+import org.neuroph.nnet.comp.neuron.InputNeuron;
 import org.neuroph.nnet.learning.BackPropagation;
 
 /**
  *
  * @author Diogo Regateiro <diogoregateiro@ua.pt>
  */
-public class ModelCreator {
+public class NeurophTest {
 
     private static final File MODEL = new File("ores.nnet");
+    private static final boolean LOAD_EXISTING_MODEL = false;
+    private static final int ITERATIONS = 100;
 
     public static void main(String[] args) {
         NeuralNetwork neuralNetwork = null;
-        if (MODEL.exists() && MODEL.isFile() && MODEL.canRead()) {
+        if (LOAD_EXISTING_MODEL && MODEL.exists() && MODEL.isFile() && MODEL.canRead()) {
             try {
                 System.out.println("Loading...");
                 neuralNetwork = MultiLayerPerceptron.load(new FileInputStream(MODEL));
             } catch (FileNotFoundException ex) {
-                Logger.getLogger(ModelCreator.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(NeurophTest.class.getName()).log(Level.SEVERE, null, ex);
                 System.exit(1);
             }
         } else {
@@ -50,29 +54,39 @@ public class ModelCreator {
             // create new perceptron network
             neuralNetwork = new NeuralNetwork();
             initNetwork(neuralNetwork);
-            neuralNetwork.setLearningRule(new BackPropagation());
 
             System.out.println("Getting dataset...");
             // create training set
-            DataSet dataset = DataSet.createFromFile("C:/Users/DiogoJosé/Documents/NetBeansProjects/FuzzyAC/java/WikipediaClient/prepDataset.csv", 8, 1, ",");
-            DataSet[] datasets = dataset.createTrainingAndTestSubsets(80, 20);
+            DataSet dataset = DataSet.createFromFile(System.getProperty("user.home") + "/Documents/NetBeansProjects/FuzzyAC/java/WikipediaClient/prepDataset.csv", 6, 1, ",");
+            dataset.shuffle();
+            DataSet[] datasets = dataset.createTrainingAndTestSubsets(75, 25);
+            System.out.println(" - Loaded " + dataset.size() + " observations.");
+            System.out.println(" - Is dataset supervised? " + dataset.isSupervised());
+            System.out.println(" - Training dataset contains " + datasets[0].size() + " observations.");
+            System.out.println(" - Validation dataset contains " + datasets[1].size() + " observations.");
 
             // learn the training set
             System.out.println("Learning...");
-            neuralNetwork.learn(datasets[0]);
+            SupervisedLearning rule = new BackPropagation();
+            rule.setNeuralNetwork(neuralNetwork);
+            for (int itr = 0; itr < ITERATIONS; itr++) {
+                rule.doOneLearningIteration(datasets[0]);
+                System.out.println(String.format(" - Iteration %03d/%03d", itr + 1, ITERATIONS));
+            }
 
             // save the trained network into file
             System.out.println("Saving...");
             neuralNetwork.save("ores.nnet");
+            System.out.println(" - Saved to ores.nnet");
 
             testNetwork(neuralNetwork, datasets[1]);
         }
         System.out.println("Done!\n");
 
-        printNetwork(neuralNetwork, 0, 0);
+        printNetwork(neuralNetwork);
     }
 
-    private static void printNetwork(NeuralNetwork neuralNetwork, double minIgnoreWeight, double maxIgnoreWeight) {
+    private static void printNetwork(NeuralNetwork neuralNetwork) {
         Map<Double, String> outputOrder = new TreeMap<>((o1, o2) -> {
             return Double.compare(Math.abs(o1), Math.abs(o2)) * -1;
         });
@@ -97,37 +111,49 @@ public class ModelCreator {
     }
 
     private static void initNetwork(NeuralNetwork neuralNetwork) {
-        Layer inputLayer = new InputLayer(8);
-        Layer middleLayer = new Layer(28);
+        System.out.println(" - Creating layers...");
+        Layer inputLayer = new InputLayer(6);
+        Layer middleLayer = new Layer(21);
         Layer outputLayer = new Layer(1);
 
-        String[] ilabels = new String[]{"DQ_OK", "DQ_ATTACK", "DQ_SPAM", "DQ_VANDALISM", "D_TRUE", "D_FALSE", "GF_TRUE", "GF_FALSE"};
+        String[] ilabels = new String[]{"DQ_OK", "DQ_ATTACK", "DQ_SPAM", "DQ_VANDALISM", "DAMAGING", "GOOD_FAITH"};
 
-        Neuron[] inputNeurons = new Neuron[8];
-
+        Neuron[] inputNeurons = new Neuron[6];
+        System.out.println(" - Creating input neurons...");
         for (int i = 0; i < ilabels.length; i++) {
-            inputNeurons[i] = new Neuron(new Min(), new Gaussian());
+            inputNeurons[i] = new InputNeuron();
             inputNeurons[i].setParentLayer(inputLayer);
             inputNeurons[i].setLabel(ilabels[i]);
             inputLayer.addNeuron(inputNeurons[i]);
         }
 
-        Neuron[][] middleNeurons = new Neuron[8][8];
+        Neuron[] dHiddenNeurons = new Neuron[inputNeurons.length];
+        System.out.println(" - Creating direct hidden neurons...");
+        for (int i = 0; i < inputNeurons.length; i++) {
+            dHiddenNeurons[i] = new Neuron(new Min(), new Gaussian());
+            dHiddenNeurons[i].setLabel("H_" + ilabels[i]);
+            dHiddenNeurons[i].setParentLayer(middleLayer);
+            middleLayer.addNeuron(dHiddenNeurons[i]);
+        }
 
+        Neuron[][] hiddenNeurons = new Neuron[inputNeurons.length][inputNeurons.length];
+        System.out.println(" - Creating 2 by 2 hidden neurons...");
         for (int i = 0; i < inputNeurons.length; i++) {
             for (int j = i + 1; j < inputNeurons.length; j++) {
-                middleNeurons[i][j] = new Neuron(new Min(), new Gaussian());
-                middleNeurons[i][j].setLabel(ilabels[i] + " + " + ilabels[j]);
-                middleNeurons[i][j].setParentLayer(middleLayer);
-                middleLayer.addNeuron(middleNeurons[i][j]);
+                hiddenNeurons[i][j] = new Neuron(new Min(), new Gaussian());
+                hiddenNeurons[i][j].setLabel(ilabels[i] + " + " + ilabels[j]);
+                hiddenNeurons[i][j].setParentLayer(middleLayer);
+                middleLayer.addNeuron(hiddenNeurons[i][j]);
             }
         }
 
+        System.out.println(" - Creating output neurons...");
         Neuron outputNeuron = new Neuron(new Min(), new Gaussian());
         outputNeuron.setLabel("OUTPUT");
         outputNeuron.setParentLayer(outputLayer);
         outputLayer.addNeuron(outputNeuron);
 
+        System.out.println(" - Setting up layers on network...");
         inputLayer.setParentNetwork(neuralNetwork);
         middleLayer.setParentNetwork(neuralNetwork);
         outputLayer.setParentNetwork(neuralNetwork);
@@ -138,24 +164,47 @@ public class ModelCreator {
         neuralNetwork.setOutputNeurons(Arrays.asList(outputNeuron));
 
         // Set connections
+        System.out.println(" - Setting up connections between neurons...");
         for (int i = 0; i < inputNeurons.length; i++) {
             for (int j = i + 1; j < inputNeurons.length; j++) {
-                neuralNetwork.createConnection(inputNeurons[i], middleNeurons[i][j], 1);
-                neuralNetwork.createConnection(inputNeurons[j], middleNeurons[i][j], 1);
-                neuralNetwork.createConnection(middleNeurons[i][j], outputNeuron, 1);
+                neuralNetwork.createConnection(inputNeurons[i], hiddenNeurons[i][j], 1);
+                neuralNetwork.createConnection(inputNeurons[j], hiddenNeurons[i][j], 1);
+                neuralNetwork.createConnection(hiddenNeurons[i][j], outputNeuron, 1);
             }
+            neuralNetwork.createConnection(inputNeurons[i], dHiddenNeurons[i], 1);
+            neuralNetwork.createConnection(dHiddenNeurons[i], outputNeuron, 1);
         }
 
+        System.out.println(" - Randomizing weights...");
         neuralNetwork.randomizeWeights();
     }
 
     public static void testNetwork(NeuralNetwork nnet, DataSet testSet) {
+        System.out.println("Evaluating...");
+        double tp = 0, tn = 0, fp = 0, fn = 0;
         for (DataSetRow dataRow : testSet.getRows()) {
             nnet.setInput(dataRow.getInput());
             nnet.calculate();
-            double[] networkOutput = nnet.getOutput();
-            System.out.print("Input: " + Arrays.toString(dataRow.getInput()));
-            System.out.println("Output: " + Arrays.toString(networkOutput));
+            double networkOutput = nnet.getOutput()[0];
+            double expectedOutput = dataRow.getDesiredOutput()[0];
+            if (expectedOutput > 0.5) {
+                if (Math.abs(expectedOutput - networkOutput) < 0.5) {
+                    tp++;
+                } else {
+                    fn++;
+                }
+            } else {
+                if (Math.abs(expectedOutput - networkOutput) < 0.5) {
+                    tn++;
+                } else {
+                    fp++;
+                }
+            }
         }
+
+        System.out.println(" - Precision: " + (tp / (tp + fp)));
+        System.out.println(" - True Positive Rate: " + (tp / (tp + fn)));
+        System.out.println(" - True Negative Rate: " + (tn / (tn + fp)));
+        System.out.println(" - Accuracy: " + ((tp + tn) / (tp + tn + fp + fn)));
     }
 }
